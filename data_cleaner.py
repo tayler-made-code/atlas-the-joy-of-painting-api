8e6c773b5fad89f5896ca2f3f4b2030279311930
F-O-P <tayler.coon@gmail.com>
2024-03-31 18:25:09 -0500

    data is cleaned as needed

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import sqlalchemy
+import ast
 
 def parse_episode_line(line):
     # split line into episode_name and rest
@@ -13,19 +13,14 @@ def parse_episode_line(line):
     # remove day from date, only keep month and year
     date = date.split(' ')[0] + ' ' + date.split(' ')[2]
 
+    # extract notes if there are any and then clean them up
     notes = date_notes_split[1] if len(date_notes_split) > 1 else None
-
-    #clean notes
     if notes:
         if notes.startswith(' - '):
             notes = notes[3:]
-        # if notes contains a '(' then add a closing ')'
         if '(' in notes and not notes.endswith(')'):
             notes = notes + ')'
-        # if not notes.endswith(')'):
-        #     notes = notes + ')'
             
-    
     return {'episode': episode_name, 'date': date, 'notes': notes}
 
 def read_episode_dates_to_df(filepath):
@@ -37,21 +32,32 @@ def read_episode_dates_to_df(filepath):
     
     return pd.DataFrame(data)
 
+def extract_subjects(row):
+    subjects = []
+    for column in subject_matter.columns[2:]:
+        if row[column] == 1:
+            subjects.append(column)
+    return subjects
 
 # Extract the data from the sources
 colors_used = pd.read_csv('data/colors_used.csv')
 subject_matter = pd.read_csv('data/subject_matter.csv')
-# extract data from the episode dates plain text file
 episode_dates = read_episode_dates_to_df('data/episode_dates.txt')
 
 
-# print each line from colors_used
-# for index, row in colors_used.iterrows():
-#     print(row)
-#     print('---')
-#     print('')
+# prints each line from colors_used
+colors_used['colors'] = colors_used['colors'].apply(lambda x: ast.literal_eval(x.replace('\r\n', '')))
+print(colors_used[['painting_index', 'painting_title', 'colors']])
+
+
+subject_matter['subjects'] = subject_matter.apply(extract_subjects, axis=1)
+print(subject_matter[['EPISODE', 'TITLE', 'subjects']])
 
 for index, row in episode_dates.iterrows():
     print(row)
     print('---')
     print('')
+
+# these are for printing to test due to the large amount of data
+# print(colors_used[['painting_index', 'painting_title', 'colors']])
+# print(subject_matter[['EPISODE', 'TITLE', 'subjects']])
